be3c84fcbd95437acf7d64330546e1a603729f17
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2022-05-02 14:03:45 +0300

    feat: done dns stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+
+  for (let item of domains) {
+    let arr = item.split('.').reverse();
+    let dm = '';
+
+    for (let el of arr) {
+      dm += '.' + el;
+
+      if (result.hasOwnProperty(dm)) {
+        result[dm] += 1;
+      } else {
+        result[dm] = 1;
+      }
+    }
+  }
+  // console.log(result);
+  return result;
 }
 
+console.log(getDNSStats(['code.yandex.ru', 'music.yandex.ru', 'yandex.ru']));
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
